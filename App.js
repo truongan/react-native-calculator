06a375398834addcd75098b2fcc8d2050acaa320
An at home workstation <hostmaster@truongan.name.vn>
2022-10-20 08:02:14 +0700

    start

--- App.js
+++ App.js
@@ -1,20 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Text, TextInput, View } from 'react-native';
 
-export default function App() {
+const PizzaTranslator = () => {
+  const [result, set_result] = useState('');
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{padding: 10}}>
+      <TextInput
+        style={{height: 40}}
+        placeholder="Type here to translate!"
+        onChangeText={newText => { 
+            try{
+              var a = eval(newText); 
+              set_result(a);
+            } catch (e){
+              set_result("");
+            }
+          }
+        }
+        defaultValue={text}
+      />
+      <Text style={{padding: 10, fontSize: 42}}>
+        {
+          result
+        }
+      </Text>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default PizzaTranslator;
