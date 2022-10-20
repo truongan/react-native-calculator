cf303d974829b16fce8f77103071179ab2d77529
truongan@acer_extensa_m2610 <truongan@acer_extensa_m2610>
2022-10-20 12:57:40 +0700

    test object

--- App.js
+++ App.js
@@ -1,23 +1,37 @@
 import React, { useState } from 'react';
-import { Text, TextInput, View } from 'react-native';
+import { Text, TextInput, View, Button } from 'react-native';
+import styled from 'styled-components/native';
+
+const Expression = styled.TextInput`
+  border-style: solid;
+  border-color: blue;
+  border-width: 2px;
+`;
 
 const PizzaTranslator = () => {
   const [result, set_result] = useState('');
+  const [expression, set_expression] = useState('');
+
+  const Calculate_button = styled.Button`
+  
+  `;
+
   return (
     <View style={{padding: 10}}>
-      <TextInput
+      <Expression
         style={{height: 40}}
         placeholder="Type here to translate!"
         onChangeText={newText => { 
-            try{
-              var a = eval(newText); 
-              set_result(a);
-            } catch (e){
-              set_result("");
-            }
+          // set_expression(newText)
+          try{
+            var a = eval(newText); 
+            set_result(a);
+          } catch (e){
+            set_result("");
           }
-        }
-        defaultValue={text}
+        }}
+        // value = {expression}
+        // defaultValue={}
       />
       <Text style={{padding: 10, fontSize: 42}}>
         {
