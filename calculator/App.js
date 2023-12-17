f97b0653d22aff7d758ba8f038c3edf19cc5213a
An at home workstation <hostmaster@truongan.name.vn>
2023-12-18 06:19:04 +0700

    get the baic to work

--- calculator/App.js
+++ calculator/App.js
@@ -1,32 +1,34 @@
 import React, { useState, useRef } from 'react';
-import { Text, TextInput, View, Button } from 'react-native';
-import { FlatList } from 'react-native-web';
+import { Text, TextInput, View, Button, StyleSheet, TouchableHighlight,FlatList } from 'react-native';
+// import { FlatList } from 'react-native-web';
 import styled from 'styled-components/native';
 
 const Expression = styled.TextInput`
+  margin-top : 20px;
   border: 3px dashed green;
 `;
 
 const Item_text_expression = styled.Text`
-  font-size: 2em;
+  /* font-size: 2em; */
   color: red;
 `
 const Item_text_result = styled.Text`
-  font-size: 3em;
+  /* font-size: 3em; */
   color: yellow;
 `
 const Item_view = styled.View`
   background-color: black;
-  padding:0.2em;
-  margin-top: 0.5em;
+  padding:2px;
+  margin-top: 5px;
 `
 const rendered_item = 
 (item) => {
   console.log("rendering ",item);
-  return <Item_view>
-    <Item_text_expression>{item.item.expression}</Item_text_expression>
-    <Item_text_result>{item.item.result}</Item_text_result>
-  </Item_view>;
+  // return <View></View>
+  return <View>
+    <Text>{item.item.expression}</Text>
+    <Text>{item.item.result}</Text>
+  </View>;
 };
 const Main_view = styled.View`
   padding-top: 20ch;
@@ -39,14 +41,19 @@ const Main_view = styled.View`
 `
 const Cal_view= styled.View`
   flex-grow: 1;
-  min-width: 20em;
-  margin-right: 1em;
+  min-width: 200px;
+  margin-right: 10px;
 `
 const History_view = styled.View`
-  margin-right: 1em;
+  margin-right: 10px;
   flex-grow: 1;
-  min-width: 20em;
+  min-width: 200px;
 `
+
+
+
+
+
 const PizzaTranslator = () => {
   const input_expression = useRef("");
 
@@ -59,60 +66,91 @@ const PizzaTranslator = () => {
     font-size: larger;
   `;
 
+  function save_history(b){
+    var a = [...history];
+    a.push( {
+      id: 'history-item' + a.length,
+      expression: input_expression.current,
+      result : b
+    } );
+
+    set_history(a);
+    set_search_result(a);
+    console.log("history hien tai la ", history);
+    console.log("search_result hien tai la ", search_result);
+
+  }
+  function calculate() {
+    try{
+      console.log("Evaluating the following expressing");
+      console.log(input_expression);
+
+      var b = eval(input_expression.current); 
+      set_result(b);
+
+      
+    } catch (e){
+      console.log(e);
+      console.log(e.stack);
+      console.log(e.line);
+      set_result("");
+      return false;
+    }
+    return true;
+  }
+
+  
   return (
+
+
     <Main_view >
       
       <Cal_view>
         
         <Expression
           placeholder="Type here to translate!"
-          onChangeText={newText =>   input_expression.current = newText     }
+          onChangeText={
+            newText =>  { 
+              input_expression.current = newText; 
+              console.log(input_expression);
+              calculate();
+              
+            }    
+          }
         />
         <Text style={{padding: 10, fontSize: 42}}>
           {
             result
           }
         </Text>
+
         <Calculate_button title="Calculate"
-          onPress = {
-            function calculate() {
-            try{
-              var b = eval(input_expression.current); 
-              set_result(b);
-              var a = [...history];
-              a.push( {
-                id: 'history-item' + a.length,
-                expression: input_expression.current,
-                result : b
-              } );
-              set_history(a);
-              set_search_result(a);
-              console.log("history hien tai la ", history);
-              console.log("search_result hien tai la ", search_result);
-            } catch (e){
-              set_result("");
+          onPress = { 
+            () => {
+              if (calculate()) save_history();
+              else {
+                console.log("No history because wrong expression");
+              }
             }
-          }}
+          }
         />
       </Cal_view>
       <History_view>
-
         <Expression 
-        placeholder="Type to search"
-        onChangeText= {search_string => {
-          var a = history.filter( (value, inedex, arr) => {
-            return value.expression.includes(search_string) || value.result.toString().includes(search_string);
-          } );
-          console.log(" Ket qua search la " , a);
-          set_search_result(a);
-        }}
-      > 
-        </Expression>
-      <FlatList
-          data={search_result}
-          renderItem = {rendered_item}
-          keyExtractor = { (item) => item.id }
+          placeholder="Type to search"
+          onChangeText= {search_string => {
+            var a = history.filter( (value, inedex, arr) => {
+              return value.expression.includes(search_string) || value.result.toString().includes(search_string);
+            } );
+            console.log(" Ket qua search la " , a);
+            set_search_result(a);
+          }}
+        /> 
+        <FlatList
+            data={search_result}
+            renderItem={rendered_item}
         />
+
       </History_view>
       
     </Main_view>
